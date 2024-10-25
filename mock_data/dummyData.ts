b4ee4edf614653fa7c142a7eb7f19b92bf4bdfe7
Arshie13 <104509367+Arshie13@users.noreply.github.com>
2024-10-25 22:44:19 +0800

    fix: fixed package-lock and package.json conflict (temporary).
    moved react-datepicker and react-day-picker to devDependencies.
    
    moved type Product in types/product

--- mock_data/dummyData.ts
+++ mock_data/dummyData.ts
@@ -1,15 +1,3 @@
-export type Product = {
-  id: number;
-  name: string;
-  category: string;
-  description: string;
-  contactPerson: string;
-  address: string;
-  contactNumber: string;
-  productGallery: string[];
-  majorProductLines: string[];
-};
-
 export const sectors = [
   "All",
   "Bamboo",
