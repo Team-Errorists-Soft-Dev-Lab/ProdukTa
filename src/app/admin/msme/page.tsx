47f278a5e6242b8a9d81083ac2bfeed612af8480
Manaf <manaf.kassim-22@cpu.edu.ph>
2024-12-11 00:21:06 +0800

    fix: change to default toast

--- src/app/admin/msme/page.tsx
+++ src/app/admin/msme/page.tsx
@@ -38,8 +38,7 @@ import {
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
-import { toast, Toaster } from "react-hot-toast";
-
+import { toast, Toaster } from "sonner";
 interface MSME {
   id: number;
   name: string;
