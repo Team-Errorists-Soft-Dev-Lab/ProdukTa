6beff9adb70ae68697f86bb52605f604d3c7cea9
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2025-05-14 15:09:00 +0800

    chore: remove outdated notes for MSME card and modal in GuestPage

--- src/app/guest/page.tsx
+++ src/app/guest/page.tsx
@@ -3,6 +3,3 @@ import GuestPageContent from "@/components/guest/GuestPageContent";
 export default function GuestPage() {
   return <GuestPageContent />;
 }
-
-// note: make a MSME card to display MSME
-// note: Make MSME Modal to page
