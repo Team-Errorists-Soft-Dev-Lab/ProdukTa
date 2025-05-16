4806c5a8d5254e8a82eab65d5e8677871460a293
bit-Matt <matthew.ledesma-22@cpu.edu.ph>
2025-05-16 09:09:02 +0800

    fix: issue where landing-page and guest page are unscrollable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className="h-full overflow-hidden">
+      <body className="h-full overflow-y-auto">
         <AuthProvider>{children}</AuthProvider>
         <Toaster richColors position="bottom-right" closeButton />
       </body>
